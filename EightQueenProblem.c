9ad2c63fb86489197ed353222ed4ce60e6ac721f
Vaibhav Singhal <iamvs2002@gmail.com>
2020-12-17 16:01:37 +0530

    Update EightQueenProblem.c

--- EightQueenProblem.c
+++ EightQueenProblem.c
@@ -1,1 +1,4 @@
-
+/*
+The Eight Queen Problem, also known as Eight Queen Puzzle, is a problem of placing eight queens on an 8 x 8 chessboard so that none of them attack one another. 
+By attacking, we mean no two are in the same row, column or diagonal.
+*/
